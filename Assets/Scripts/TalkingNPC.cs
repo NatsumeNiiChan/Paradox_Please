48a6b9174d6b21fc9602a9a8f187e534d86d8c65
Akira <natsumeniichan@gmail.com>
2023-03-06 11:29:27 +0100

    Textbox & Teleport Script

--- Assets/Scripts/TalkingNPC.cs
+++ Assets/Scripts/TalkingNPC.cs
@@ -7,24 +7,54 @@ using TMPro;
 public class TalkingNPC : MonoBehaviour
 {
     public GameObject TextBox;
+    public GameObject TextField;
 
-    private void Start()
+    public string[] Texts;
+
+    public string texts;
+
+    int count = 0;
+
+    private void Awake()
     {
-        
+        Texts = new string[]
+        {
+            "Hello",
+            "Nice Weather!",
+            "Blub"
+        };
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && count < Texts.Length)
+        {
+            count++;
+        }
+
+        texts = Texts[count];
+        GetText();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         TextBox.SetActive(true);
+        TextField.SetActive(true);
+
+        count = 0;
     }
 
-    public void GetText()
+    private void OnTriggerExit2D(Collider2D collision)
     {
+        TextBox.SetActive(false);
+        TextField.SetActive(false);
+    }
 
+    private void GetText()
+    {
+        if (TextBox != null)
+        {
+            TextBox.GetComponent<TMP_Text>().text = texts;
+        }
     }
 }
