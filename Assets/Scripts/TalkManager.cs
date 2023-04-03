749cc1eebfd1e381ecf6e331cfd4b038fd63ff75
Akira <natsumeniichan@gmail.com>
2023-04-03 11:21:17 +0200

    Text Script Fix

--- Assets/Scripts/TalkManager.cs
+++ Assets/Scripts/TalkManager.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
 
 public class TalkManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string Name;
+
+    public LocalizeStringEvent localizedStringEvent;
+    private LocalizedString originalLocalizedString;
+
+    private int i = 1;
+    private string keyText;
+    public int StringCount;
+
+    public LocalizedString localizedString = new LocalizedString{};
+    public string TableName = "NPC";
+    private string KeyName;
+
+    public GameObject TextBox;
+
+    private void Start()
     {
-        
+        originalLocalizedString = localizedStringEvent.StringReference;
+
+        localizedStringEvent.OnUpdateString.AddListener(OnStringChanged);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnStringChanged(string s)
     {
-        
+        Debug.Log("String changed");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            localizedStringEvent.StringReference = originalLocalizedString;
+
+            SetKeyName();
+
+            localizedStringEvent.StringReference.SetReference(TableName, KeyName);
+
+            i++;
+        }
+    }
+
+    private void SetKeyName()
+    {
+        if (i <= StringCount)
+        {
+            KeyName = "Text " + i;
+        }
+
+        else
+        {
+            i = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TextBox.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        TextBox.SetActive(false);
     }
 }
